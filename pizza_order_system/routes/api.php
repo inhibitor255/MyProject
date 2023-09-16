8239e0ff54d51512c9e222cc4d8b2fdd5ee4e371
Kaung Myat <kaungmyatkyaw255@example.com>
2023-09-16 14:55:44 +0630

    pizza_order_system Day-32

--- pizza_order_system/routes/api.php
+++ pizza_order_system/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\CategoryApiController;
 use App\Http\Controllers\ContactApiController;
 use App\Http\Controllers\ProductApiController;
 use App\Http\Controllers\RouteController;
+use Dotenv\Store\File\Reader;
+use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,3 +32,31 @@ Route::apiResource('/contacts', ContactApiController::class);
 Route::post('category/delete', [RouteController::class, 'deleteCategory']);
 Route::post('category/details', [RouteController::class, 'showCategory']);
 Route::post('category/update', [RouteController::class, 'updateCategory']);
+
+
+//For Category
+    // {Create
+    // http://127.0.0.1:8000/api/categories with Post method ( need name wants to create);
+
+    // Read
+    // http://127.0.0.1:8000/api/categories/ with Get method
+
+    // Read Detail
+    // http://127.0.0.1:8000/api/categories/{id} need (id)
+
+    // Update
+    // http://127.0.0.1:8000/api/category/update with Post method need(id, name wants to update)
+
+    // Delete
+    // http://127.0.0.1:8000/api/category/delete with Post method need (id wants to delete)}
+
+//For Product
+    // Read
+    // http://127.0.0.1:8000/api/products with Get method
+
+// Contact
+    // Create
+    // http://127.0.0.1:8000/api/contacts with Post method (need name, email, message want to create)
+
+    // Read
+    // http://127.0.0.1:8000/api/contacts with Get method
